9ddbe40749e49da2ea31a70825b1ddb50448a179
mfngoi <mfngoi3@gmail.com>
2024-12-03 19:44:41 -0800

    feat: add button to modify canvas size
    Button can be used to reset and resize the canvas up to 100x100.

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const COLUMN_LENGTH = 16;
 // Document Objects
 const canvas = document.querySelector(".canvas");
 const canvasStyle = window.getComputedStyle(canvas);
+const button = document.querySelector("button");
 
 let canvasSize = parseFloat(canvasStyle.width);
 let dotSize = canvasSize / ROW_LENGTH;
 console.log(`Canvas: ${canvasSize}, Dot: ${dotSize}`);
-
 
 for(let row=0; row<ROW_LENGTH; row++) {
     for(let col=0; col<COLUMN_LENGTH; col++) {
@@ -26,3 +26,31 @@ for(let row=0; row<ROW_LENGTH; row++) {
         canvas.appendChild(dot);
     }    
 }
+
+button.addEventListener("click", () => {
+
+    let size = parseFloat(prompt("Set canvas size:", "16"));
+    while(size > 100)
+     size = parseFloat(prompt("Max canvas size is 100!!! \nSet canvas size:", "100"));
+
+    while(canvas.lastElementChild) {
+        canvas.removeChild(canvas.lastElementChild)
+    }
+
+    let dotSize = canvasSize / size;
+    for(let row=0; row<size; row++) {
+        for(let col=0; col<size; col++) {
+            let dot = document.createElement("div");
+            dot.classList.add("dot");
+            dot.style["width"] = dotSize + "px";
+            dot.style["height"] = dotSize + "px";
+    
+            dot.addEventListener("mouseover", () => {
+                dot.style["background-color"] = "red";
+            });
+    
+            canvas.appendChild(dot);
+        }    
+    }
+
+});
