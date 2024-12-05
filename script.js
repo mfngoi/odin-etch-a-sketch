27761e5047a85537023d6a139a02754030d2bf69
mfngoi <mfngoi3@gmail.com>
2024-12-05 10:45:15 -0800

    feat: add randomized RGB colors to each square

--- script.js
+++ script.js
@@ -12,45 +12,51 @@ let canvasSize = parseFloat(canvasStyle.width);
 let dotSize = canvasSize / ROW_LENGTH;
 console.log(`Canvas: ${canvasSize}, Dot: ${dotSize}`);
 
-for(let row=0; row<ROW_LENGTH; row++) {
-    for(let col=0; col<COLUMN_LENGTH; col++) {
+for (let row = 0; row < ROW_LENGTH; row++) {
+    for (let col = 0; col < COLUMN_LENGTH; col++) {
         let dot = document.createElement("div");
         dot.classList.add("dot");
         dot.style["width"] = dotSize + "px";
         dot.style["height"] = dotSize + "px";
 
-        dot.addEventListener("mouseover", () => {
-            dot.style["background-color"] = "red";
+        dot.addEventListener("mouseover", (event) => {
+            let rValue = Math.floor(Math.random() * 256);
+            let gValue = Math.floor(Math.random() * 256);
+            let bValue = Math.floor(Math.random() * 256);
+            dot.style["background-color"] = `rgb(${rValue}, ${gValue}, ${bValue})`;
         });
 
         canvas.appendChild(dot);
-    }    
+    }
 }
 
 button.addEventListener("click", () => {
 
     let size = parseFloat(prompt("Set canvas size:", "16"));
-    while(size > 100)
-     size = parseFloat(prompt("Max canvas size is 100!!! \nSet canvas size:", "100"));
+    while (size > 100)
+        size = parseFloat(prompt("Max canvas size is 100!!! \nSet canvas size:", "100"));
 
-    while(canvas.lastElementChild) {
+    while (canvas.lastElementChild) {
         canvas.removeChild(canvas.lastElementChild)
     }
 
     let dotSize = canvasSize / size;
-    for(let row=0; row<size; row++) {
-        for(let col=0; col<size; col++) {
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
             let dot = document.createElement("div");
             dot.classList.add("dot");
             dot.style["width"] = dotSize + "px";
             dot.style["height"] = dotSize + "px";
-    
+
             dot.addEventListener("mouseover", () => {
-                dot.style["background-color"] = "red";
+                let rValue = Math.floor(Math.random() * 256);
+                let gValue = Math.floor(Math.random() * 256);
+                let bValue = Math.floor(Math.random() * 256);
+                dot.style["background-color"] = `rgb(${rValue}, ${gValue}, ${bValue})`;
             });
-    
+
             canvas.appendChild(dot);
-        }    
+        }
     }
 
 });
